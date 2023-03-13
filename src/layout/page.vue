bbb19180cbc3cf55e57869401629c241c9ca46db
raulito <raul_0.4_@hotmail.com>
2023-03-13 23:18:21 +0100

    add css to detailview and make navbar fix

--- src/layout/page.vue
+++ src/layout/page.vue
@@ -5,8 +5,8 @@ import NavBar from '../components/page/NavBar.vue'
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <NavBar />
-    <div class="flex-1 w-full flex flex-col">
+    <NavBar class="fixed bg-slate-50 shadow-md z-50 w-full flex justify-between items-center" />
+    <div class="flex-1 w-full flex flex-col pt-10">
       <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
         <slot />
       </div>
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -22,29 +22,43 @@ let urlImg = computed(() => {
 </script>
 
 <template>
-  <div class="py-10 px-5 flex justify-between flex-col md:flex-row">
-    <div class="w-full md:w-5/12 rounded-lg bg-slate-50">
+  <div class="py-5 md:py-10 px-2 md:px-20 flex justify-between flex-col md:flex-row">
+    <div class="w-full md:w-5/12 rounded-lg bg-slate-50 h-2/3">
       <img
         :src="urlImg"
         class="h-60 md:h-80 w-full overflow-hidden bg-cover bg-no-repeat rounded-t-lg"
         alt="imgNotFound"
       />
-      <div class="hover:bg-blue-500 hover:text-white text-center py-2 rounded-b-lg cursor-pointer">
-        vota aqui
+      <div class="text-center rounded-b-lg cursor-pointer flex justify-center">
+        <p class="px-3 py-5 hover:bg-blue-500 hover:text-white w-4/12 rounded-b">
+          Average {{ getTvShowData.vote_average }}
+        </p>
+        <p class="px-3 py-5 hover:bg-blue-500 hover:text-white w-4/12 rounded-b">
+          Votes {{ getTvShowData.vote_count }}
+        </p>
+        <p class="px-3 py-5 hover:bg-blue-500 hover:text-white w-4/12 rounded-b">
+          Popularity {{ getTvShowData.popularity }}
+        </p>
       </div>
     </div>
-    <div class="w-6/12">
-      <h1>name: {{ getTvShowData.name }}</h1>
-      <p>first_air_date: {{ getTvShowData.first_air_date }}</p>
-      <p>number_of_episodes: {{ getTvShowData.number_of_episodes }}</p>
-      <p>overview: {{ getTvShowData.overview }}</p>
-      <p>vote_average: {{ getTvShowData.vote_average }}</p>
-      <p>vote_count: {{ getTvShowData.vote_count }}</p>
-      <p>status: {{ getTvShowData.status }}</p>
-      <p>{{ getTvShowData.name }}</p>
+    <div class="md:w-6/12 md:px-2">
+      <h1 class="text-6xl text-center pb-6">{{ getTvShowData.name }}</h1>
+      <p class="text-lg pb-6 text-justify">{{ getTvShowData.overview }}</p>
+
+      <div class="flex justify-center">
+        <p class="px-3 py-5 border-b border-t rounded-lg hover:shadow-lg w-4/12 rounded-b">
+         <div> Date:</div> {{ getTvShowData.first_air_date }}
+        </p>
+        <p class="px-3 py-5 border-b border-t rounded-lg hover:shadow-lg  w-4/12 rounded-b">
+          <div> Seasons:</div> {{ getTvShowData.number_of_seasons }}
+        </p>
+        <p class="px-3 py-5 border-b border-t rounded-lg hover:shadow-lg  w-4/12 rounded-b">
+         <div> Episodes: </div>{{ getTvShowData.number_of_episodes }}
+        </p>
+      </div>
     </div>
   </div>
-  <div class="flex flex-wrap justify-start pb-10 px-5">
+  <div class="flex flex-wrap justify-start py-5 md:py-10 px-2 md:px-20">
     <div v-for="season in getTvShowData.seasons" class="px-2 w-40">
       <p>{{ season.name }}</p>
       <img
